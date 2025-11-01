3d003b7ba556b78a55f4a609a3f871c8e33c64dc
JoaoVictor_Moraes <joaovictor.mroaes2728@gmail.com>
2025-11-01 20:36:19 -0300

    section de contatos e conversa quase finalizada, iniciar lógica das mensagems no JS e estilização de icones com o hover

--- modulo/app.js
+++ modulo/app.js
@@ -1,13 +1,45 @@
 'use strict'
 
-
-
 async function loadContacts() {
     let url = 'https://api-zapzap.onrender.com/v1/messages/11987876567'
     let response = await fetch(url)
 
     let contacts = await response.json()
-    console.log(contacts.messages)
+    createContacts(contacts.messages[0])
 }
 
-loadContacts()
+function createContacts(contacts) {
+    const sectionConversas = document.getElementById('sectionConversas')
+    
+    contacts.forEach((contact) => {
+        const divContato = document.createElement('div')
+        const divImg = document.createElement('div')
+        const img = document.createElement('img')
+        const divInf = document.createElement('div')
+        const pNome = document.createElement('p')
+        const pUltimaMensagem = document.createElement('p')
+        const pData = document.createElement('p')
+
+        sectionConversas.appendChild(divContato)
+        divContato.append(divImg, divInf, pData)
+        divImg.appendChild(img)
+        divInf.append(pNome, pUltimaMensagem)
+
+        divContato.classList.add('contato')
+        divImg.classList.add('imgPerfil')
+        divInf.classList.add('informacao')
+        pNome.classList.add('nome')
+        pUltimaMensagem.classList.add('ultimaMensagem')
+        pData.classList.add('data')
+
+        let lastMessage = contact.messages.length - 1
+
+        // img.src = contact.image
+        pNome.innerHTML = contact.name
+        pUltimaMensagem.innerHTML = contact.messages[lastMessage].content
+        pData.innerHTML = contact.messages[lastMessage].time
+    });
+
+}
+
+// loadContacts()
